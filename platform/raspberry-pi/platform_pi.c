672fa4bc05f08d66357998edc78f89406fe830e8
rakhilpe <rakhilpe001@gmail.com>
2024-11-04 23:35:34 +0530

    Updated License and RDK Apache header

--- platform/raspberry-pi/platform_pi.c
+++ platform/raspberry-pi/platform_pi.c
@@ -1,3 +1,32 @@
+/************************************************************************************
+  If not stated otherwise in this file or this component's LICENSE file the
+  following copyright and licenses apply:
+
+  Copyright 2024 RDK Management
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+ **************************************************************************/
+
+
+/* Adapted code from hostap, which is:
+Copyright (c) 2002-2015, Jouni Malinen j@w1.fi
+Copyright (c) 2003-2004, Instant802 Networks, Inc.
+Copyright (c) 2005-2006, Devicescape Software, Inc.
+Copyright (c) 2007, Johannes Berg johannes@sipsolutions.net
+Copyright (c) 2009-2010, Atheros Communications
+Licensed under the BSD-3 License
+*/
+
 #include <stddef.h>
 #include <string.h>
 #include <stdlib.h>
